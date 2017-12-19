6c1f46b6ded54486a16565015827c3698fec887e
Esther Weon <esther.s.weon@gmail.com>
2017-12-19 10:43:15 -0800

    Feedback

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-// require express and other modules
 var express = require('express'),
-    app = express();
+    app = express(),
+    bodyParser = require('body-parser');
 
-// parse incoming urlencoded form data
-// and populate the req.body object
-var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // allow cross origin requests (optional)
@@ -43,83 +40,78 @@ app.get('/', function homepage(req, res) {
  */
 
 app.get('/api', function apiIndex(req, res) {
-  // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
-  // It would be seriously overkill to save any of this to your database.
-  // But you should change almost every line of this response.
   res.json({
     woopsIForgotToDocumentAllMyEndpoints: false, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
     documentationUrl: "https://github.com/SwethaMuralidharan/express-personal-api/blob/master/README.md", // CHANGE ME
     baseUrl: "https://enigmatic-caverns-31537.herokuapp.com/", // CHANGE ME
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"}, // done
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // done
+      { 
+        method: "GET", 
+        path: "/api", 
+        description: "Describes all available endpoints"
+      },
+      { 
+        method: "GET", 
+        path: "/api/profile", 
+        description: "Data about me"
+      },
 
-      {method: "GET", path: "/api/profile/projects", decsription: "Describes all my projects."}, // done
-      {method: "GET", path: "/api/profile/:profile_id/projects/:_id",description: "displays particular project info"}, //done
-      {method: "POST",path: "/api/profile/:profile_id/projects",description:"creates new project"}, //done
-      {method: "DELETE", path:"/api/profile/:profile_id/projects/:_id", description:"deletes that project"},//done
-      {method: "PUT", path:"/api/profile/:profile_id/projects/:_id", description:"Updates that project by ID"},//done
+      { 
+        method: "POST",
+        path: "/api/profile/:profile_id/projects",
+        description:"creates new project"
+      },
+      { 
+        method: "DELETE", 
+        path:"/api/profile/:profile_id/projects/:_id", 
+        description:"deletes that project"
+      },
+      { 
+        method: "PUT", 
+        path:"/api/profile/:profile_id/projects/:_id", 
+        description:"Updates that project by ID"
+      },
 
-      {method: "GET", path: "/api/profile/:profile_id/vacation",description:"destinations i've visited"},//done
-      {method: "POST",path:"/api/profile/:profile_id/vacation",description:"Creates an entry for vacations under planning"}
-
+      { 
+        method: "GET", 
+        path: "/api/profile/:profile_id/vacation",
+        description:"destinations i've visited"
+      },
+      { 
+        method: "POST",
+        path:"/api/profile/:profile_id/vacation",
+        description:"Creates an entry for vacations under planning"
+      }
     ]
   })
 });
 
-/*End Point 2 - Get Profile Info*/
 app.get('/api/profile', function show(req, res) {
-  console.log("inside get profile",req.body);
   db.Profile.find({},function(err,new_profile){
     if(err) { console.log(" new profile not exist", err)}
     res.json(new_profile);
   })
 });
 
-/* End point 3 - Get All Projects */
-app.get('/api/profile/projects', function(req,res){
-  console.log("inside get projects", req);
-  db.Profile.find({},function(err,projects){
-    if(err){ console.log("Get All projects Method Error",err);}
-    res.json(projects);
-  })
-});
-
-/* End Point 4 - Create new project*/
 app.post('/api/profile/:profile_id/projects',function(req,res){
-  console.log(`inside create projects method ${req}`);
-  console.log(JSON.stringify(req.body));
-  db.Profile.findById(req.params.profile_id,function(err,foundProfile){
+  db.Profile.findById(req.params.profile_id, function(err,foundProfile){
+    var newProject = new db.Projects(req.body);
 
-  var newProject=new db.Projects(req.body);
-
-  foundProfile.projects.push(newProject);
-  foundProfile.save(function(err,savedprofile){
-    res.json(savedprofile);
+    foundProfile.projects.push(newProject);
+    foundProfile.save(function(err,savedProfile){
+      res.json(savedProfile);
+    });
   })
-})
 });
 
-/* End Point 5 - Show one project by ID*/
-app.get('/api/profile/:profile_id/projects/:_id',function(req,res){
-  console.log("inside getbyID");
-  db.Profile.findById(req.params._id, function(err, foundproj) {
-    res.json(foundproj);
-  });
-
-})
-
-/*End Point 6 - delete project by ID*/
 app.delete("/api/profile/:profile_id/projects/:_id",function destroy(req, res) {
-  db.Profile.findById(req.params.profile_id, function(err, foundproj) {
-    console.log(foundproj);
-
-    var correctProj = foundproj.projects.id(req.params._id);
+  db.Profile.findById(req.params.profile_id, function(err, foundProfile) {
+    var correctProj = foundProfile.projects.find(proj => proj._id == req.params._id);
     if (correctProj) {
       correctProj.remove();
 
-      foundproj.save(function(err, saved) {
+      foundProfile.save(function(err, saved) {
         console.log('REMOVED ', correctProj.name);
         res.json(correctProj);
       });
@@ -127,13 +119,11 @@ app.delete("/api/profile/:profile_id/projects/:_id",function destroy(req, res) {
   });
 });
 
-/* End Point 7 - update project*/
 app.put("/api/profile/:profile_id/projects/:_id",function update(req, res) {
   db.Profile.findById(req.params.profile_id, function(err, foundProfile) {
     var correctProj = foundProfile.projects.id(req.params._id);
 
     if (correctProj) {
-
       correctProj.name = req.body.name;
       correctProj.description = req.body.description;
       correctProj.project_url=req.body.project_url;
@@ -147,35 +137,27 @@ app.put("/api/profile/:profile_id/projects/:_id",function update(req, res) {
   });
 });
 
-// End Point 8 - Add New vacation under planning
-
 app.post('/api/profile/:profile_id/vacation',function(req,res){
-  console.log(JSON.stringify(req.body));
   db.Profile.findById(req.params.profile_id,function(err,foundProfile){
+    var newVacation=new db.Vacation(req.body);
 
-  var newVacation=new db.Vacation(req.body);
-
-  foundProfile.vacation.push(newVacation);
-  foundProfile.save(function(err,savedprofile){
-    res.json(savedprofile);
-  })
-})
+    foundProfile.vacation.push(newVacation);
+    foundProfile.save(function(err,savedProfile){
+      res.json(savedProfile);
+    });
+  });
 });
 
-//End Point 9 - Get All Holiday destinations
-
 app.get('/api/profile/:profile_id/vacation',function(req,res){
-  db.Profile.find({},function(err,vacation){
+  db.Profile.findById(req.params.profile_id,function(err,foundProfile){
     if(err){ console.log("Get All projects Method Error",err);}
-    res.json(vacation);
+    res.json(foundProfile.vacation);
   })
 })
 
 /**********
  * SERVER *
  **********/
-
-// listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is up and running on http://localhost:3000/');
